fa5cae78d36e26a7029bbd53800bb25d2db761a4
Justin Kim <naznsan@gmail.com>
2020-09-21 01:50:47 +1200

    Add basic grid and user input for creating maze

---
+++ App.js
@@ -0,0 +1,51 @@
+let grid = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+function toggleCell(x, y) {
+    if (grid[x][y] == 0) {
+        grid[x][y] = 1;
+    } else {
+        grid[x][y] = 0;
+    }
+}
+
+function drawGrid() {
+    // Remove existing grid
+    let existingTable = document.getElementsByTagName("table")[0];
+    if (existingTable) existingTable.remove();
+
+    // Draw new grid
+    let table = document.createElement("table");
+    table.style.height = "500px";
+    table.style.width = "500px";
+    for (let i = 0; i < 10; i++) {
+        let row = table.insertRow(i);
+        for (let j = 0; j < 10; j++) {
+            let cell = row.insertCell(j);
+            if (grid[i][j] == 1) {
+                cell.style.backgroundColor = "black";
+            }
+            cell.addEventListener("click", event => {
+                toggleCell(i, j);
+                drawGrid();
+            });
+        }
+    }
+    document.body.appendChild(table);
+}
+
+function main() {
+    drawGrid();
+}
+
+main();
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # algorithmMaze
 DFS &amp; BFS algorithms implemented in an user-created maze
+
+## Versions
+### v0.1
+Added a grid that displays the maze, and changes the maze based on user input
+![v0.1 screenshot](readme-resources/v0.1.gif)
